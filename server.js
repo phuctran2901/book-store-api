02b899c7936f32ed082f3213ffb5b5be95e97512
hoang phuc <hp.sliver123@gmail.com>
2021-06-06 15:59:10 +0700

    fix port heroku

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('*', function (req, res, next) {
 })
 
 
-const port = process.env.APP_PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`server running is port ${port}`)
