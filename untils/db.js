091bdefe7a8f6fca601e5d2dab1c723299f49f6b
phuctran <hp.sliver123@gmail.com>
2023-04-28 09:00:39 +0700

    change db

--- untils/db.js
+++ untils/db.js
@@ -1,20 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.DB_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        })
-        console.log("DB connect successfully");
-    }
-    catch (err) {
-        console.log(err);
-        console.log("DB connect failed")
-        process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect(process.env.DB_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    })
+    console.log('DB connect successfully')
+  } catch (err) {
+    console.log(err)
+    console.log('DB connect failed')
+    process.exit(1)
+  }
 }
 
-module.exports = { connectDB };
+module.exports = { connectDB }
